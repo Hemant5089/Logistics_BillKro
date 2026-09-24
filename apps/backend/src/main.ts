0fefd466e01882f04f67e66e45130e396da0dc76
Hemant Vaishnav <159165750+Hemant5089@users.noreply.github.com>
2026-09-24 13:00:13 +0530

    Fix production CORS

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,10 @@
-import { Reflector } from '@nestjs/core';
-
-import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
+import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { Reflector } from '@nestjs/core';
+
 import { AppModule } from './app.module';
+import { JwtAuthGuard } from './modules/auth/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
     origin: [
       'http://localhost:3001',
       'http://127.0.0.1:3001',
+      'https://logistics-bill-kro.vercel.app',
+      'https://logistics-bill-4iu3gttn9-team-billkro.vercel.app',
     ],
     credentials: true,
   });
